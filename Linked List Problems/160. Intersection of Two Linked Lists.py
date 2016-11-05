0ce267debf1151cf0d0a34e26a9e860102ceda56
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2016-11-04 19:13:16 -0600

    problem 160

---
+++ Linked List Problems/160. Intersection of Two Linked Lists.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+import gc
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        gc.collect()
+        pa,pb = headA,headB
+        while pa is not pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+        return pa
+        
