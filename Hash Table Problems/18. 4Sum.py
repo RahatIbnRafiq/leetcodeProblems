2f282e9d662f5f86b4fc7fd59587a83175670db2
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2016-11-09 09:35:22 -0700

    4-Sum

---
+++ Hash Table Problems/18. 4Sum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        def helper(nums, target, N, result, results):
+            if len(nums)<N or N<2 or target < nums[0]*N or target > nums[-1]*N:
+                return
+            if N==2:
+                left,right = 0,len(nums)-1
+                while left < right:
+                    s = nums[left]+nums[right]
+                    if s == target:
+                        results.append(result+[nums[left],nums[right]])
+                        left+=1
+                        while left < right and nums[left]==nums[left-1]:
+                            left+=1
+                    elif s < target:
+                        left+=1
+                    else:
+                        right-=1
+            else:
+                for i in range(len(nums)):
+                    if i == 0 or (i>0 and nums[i]!=nums[i-1]):
+                        helper(nums[i+1:],target-nums[i],N-1,result+[nums[i]],results)
+        results = []
+        helper(sorted(nums),target,4,[],results)
+        return results
+                    
+            
+        
+        
