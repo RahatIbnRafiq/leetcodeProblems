4720267cea51808026c349ed0819a4ee2d09b017
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2016-11-13 09:15:43 -0700

    find the duplicate number

--- Two Pointers/80. Remove Duplicates from Sorted Array II.py
+++ Two Pointers/80. Remove Duplicates from Sorted Array II.py
@@ -6,7 +6,7 @@ class Solution(object):
             if i < 2 or n > nums[i-1] or n > nums[i-2]:
                 nums[i] = n
                 i+=1
-        print nums[:i]
+        return i
 
 
 s = Solution()
