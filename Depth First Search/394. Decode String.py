72855f9d172cd329f96837dd14746284f43d42d3
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2017-02-05 21:30:59 -0700

    dfs

---
+++ Depth First Search/394. Decode String.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def decodeString(self, s):
+        stack = []
+        stack.append(["", 1])
+        num = ""
+        for ch in s:
+            if ch.isdigit():
+                num +=ch
+            elif ch == "[":
+                stack.append(["",int(num)])
+                num = ""
+            elif ch=="]":
+                st,k = stack.pop()
+                stack[-1][0] += st*k
+            else:
+                stack[-1][0]+=ch
+        return stack[-1][0]
+
+
+s = Solution()
+s.decodeString("3[a2[c]]")
