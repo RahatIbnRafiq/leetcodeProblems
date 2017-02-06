1568028f72cf8095b2866c0cb6a0b7c7e95a32c8
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2017-02-05 20:27:42 -0700

    dfs

---
+++ Depth First Search/494. Target Sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        if not nums:return 0
+        dic = {nums[0]:1,-nums[0]:1} if nums[0]!=0 else{0:2}
+        for i in range(1,len(nums)):
+            temp = {}
+            for d in dic:
+                temp[d+nums[i]] = temp.get(d+nums[i],0)+dic.get(d,0)
+                temp[d-nums[i]] = temp.get(d-nums[i],0)+dic.get(d,0)
+            dic = temp
+        return dic.get(S,0)
+
+
+s = Solution()
+print s.findTargetSumWays([1, 1, 1, 1, 1],3)
+        
+        
